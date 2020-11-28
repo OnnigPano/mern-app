d24f01606903fb1a72f0c02913f60679e28eac7a
OnnigPano <onnigpano@gmail.com>
2020-11-28 19:40:24 -0300

    ready to deploy

--- client/build/precache-manifest.9cfa55bb8836c40b75a341a51f7944c0.js
+++ client/build/precache-manifest.9cfa55bb8836c40b75a341a51f7944c0.js
@@ -1,18 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-<<<<<<< HEAD:client/build/precache-manifest.b44f244f539526a5eeaeaab6d1f598f2.js
-    "revision": "899ea7db39dd5ef83bf6ffc7debbd6f9",
-    "url": "/index.html"
-  },
-  {
-    "revision": "84480340ff8a54f34245",
-=======
     "revision": "87d95ee8875f48b1c2c86107b56512d2",
     "url": "/index.html"
   },
   {
     "revision": "e935d42dfd32fbe707f4",
->>>>>>> 20364c4965d03d3b69bfd0927f46c5ef22b085c4:client/build/precache-manifest.9cfa55bb8836c40b75a341a51f7944c0.js
     "url": "/static/css/main.5facb584.chunk.css"
   },
   {
@@ -24,13 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.718cf95c.chunk.js.LICENSE.txt"
   },
   {
-<<<<<<< HEAD:client/build/precache-manifest.b44f244f539526a5eeaeaab6d1f598f2.js
-    "revision": "84480340ff8a54f34245",
-    "url": "/static/js/main.f235eb39.chunk.js"
-=======
     "revision": "e935d42dfd32fbe707f4",
     "url": "/static/js/main.3e927837.chunk.js"
->>>>>>> 20364c4965d03d3b69bfd0927f46c5ef22b085c4:client/build/precache-manifest.9cfa55bb8836c40b75a341a51f7944c0.js
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
