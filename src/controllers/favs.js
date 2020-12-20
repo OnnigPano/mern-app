4ef548547ff0dd85666c98a4bf39d2c05575e324
OnnigPano <onnigpano@gmail.com>
2020-12-20 16:41:13 -0300

    Added getAllFavs and deleteFromFavs

--- src/controllers/favs.js
+++ src/controllers/favs.js
@@ -1,5 +1,5 @@
 const Product = require('../models/product');
-
+const ObjectId = require('mongoose').Types.ObjectId;
 
 
 const favsController = {
@@ -10,9 +10,9 @@ const favsController = {
             if (!product) {
                 return res.status(404).json({ error: 'Product not Found' });
             }
-            let productAlreadyInFavs = req.user.favs.find((favItem) => favItem == product._id);
+            let productAlreadyInFavs = req.user.favs.find((favItem) => favItem.toString() === product._id.toString());
             if (productAlreadyInFavs) {
-                return res.status(200).json(product);
+                return res.status(200).json({ error: 'Product already in user favs' });
             }
             req.user.favs.push(product);
             await req.user.save();
@@ -20,6 +20,25 @@ const favsController = {
             res.status(200).json(req.user.favs);
         } catch (e) {
             res.status(404).json(e);
+        }
+    },
+    deleteFromFavs: async (req, res) => {
+        try {
+            req.user.favs = req.user.favs.filter((productId) => new ObjectId(productId).toString() !== req.params.id);
+            await req.user.save();
+
+            res.status(200).json(req.user.favs);
+        } catch (e) {
+            res.status(400).send(e);
+        }
+    },
+    getAllFavs: async (req, res) => {
+        try {
+            let populatedFavs = await req.user.populate({path: 'favs', model: 'Product'}).execPopulate();   
+            
+            res.status(200).json(populatedFavs);
+        } catch (e) {
+            res.status(400).send(e);
         }
     }
 }
--- src/routes/favs.js
+++ src/routes/favs.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const favsController = require('../controllers/favs');
 const auth = require('../middleware/auth');
 
+router.get('/favs', auth, favsController.getAllFavs);
 router.post('/favs/:id', auth, favsController.addToFavs);
+router.delete('/favs/:id', auth, favsController.deleteFromFavs);
 
 module.exports = router;
